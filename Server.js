da53f8d968a2da55611c5a00480c6e113b4afdfd
Kalayu-Ftsum <kalayuftsumofficial@gmail.com>
2023-09-26 18:02:02 +0200

    Implemented token bucket for rate limiter

--- Server.js
+++ Server.js
@@ -1,16 +1,48 @@
-const express=require('express');
+const express = require('express');
 
-const app=express();
+const app = express();
 
-const PORT=3000;
-app.get('/unlimited',(req,res)=>{
+const PORT = 3000;
+app.get('/unlimited', (req, res) => {
     res.send('Unlimited! Let\'s Go!')
 })
 
-app.get('/limited',(req,res)=>{
+let buckets = {}
+const LIMIT=2;
+const updateTime=0.01 // in seconds
+
+app.get('/limited', (req, res) => {
+    const ipAddress = req.header('x-forwarded-for') || req.socket.remoteAddress;
+    
+    if (buckets[ipAddress] !== undefined) {
+        
+        
+        console.log('buckets',buckets)
+
+        const token=buckets[ipAddress].token
+        const currentTime=new Date()
+        const timeDiff=Math.floor((currentTime - buckets[ipAddress].ts)/1000)
+        console.log({timeDiff})
+        console.log({now:new Date()})
+        buckets[ipAddress].token=Math.min((token + timeDiff*updateTime),LIMIT)
+
+        if(buckets[ipAddress].token <= 1 ){
+            return  res.status(429).send('Too Many Requests.')
+        }
+        buckets[ipAddress].token -= 1
+        
+    } else {
+        buckets[ipAddress] ={
+            token:LIMIT,
+            ts:new Date()
+        }
+    }
+
+
+
     res.send('Limited, don\'t over use me!')
 })
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 });
