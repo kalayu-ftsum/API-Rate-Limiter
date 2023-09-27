6521ebb6cbd492d9f508e86716cba2442d981840
Kalayu-Ftsum <kalayuftsumofficial@gmail.com>
2023-09-27 17:03:24 +0200

    Refactored fixed window counter class

--- utils/windowCounter.js
+++ utils/windowCounter.js
@@ -1,31 +1,37 @@
-class WindowCounter{
-    constructor(threshold,windowSize){
-        this.threshold=threshold
-        this.windowSize=windowSize
-        this.windowcounter=0
-        this.windowTime=new Date().getTime()
+class WindowCounter {
+    constructor(threshold, windowSize) {
+        this.threshold = threshold
+        this.windowSize = windowSize
+        this.windowcounter = 0
+        this.windowTime = new Date().getTime()
     }
-    updateWindow(){
-     let currentTime=new Date().getTime()
-
-     let window= Math.floor((currentTime - this.windowTime)/(this.windowSize*1000))
-     if(window ==0){
-        if( this.windowcounter < this.threshold){
-            this.windowcounter+=1
-            return false
-        }else{
-            return true 
+    updateWindow() {
+        if (this.getCurrentWindow() == 0) {
+            return this.isThresholdReached()
         }
-     }else{
         this.resetCounter()
-        this.windowcounter+=1
+        this.increaseCounter()
         return false
-     }
     }
-    resetCounter(){
-        this.windowcounter=0
-        this.windowTime= new Date().getTime()
+    getCurrentWindow() {
+        let currentTime = new Date().getTime()
+
+        return Math.floor((currentTime - this.windowTime) / (this.windowSize * 1000))
+    }
+    isThresholdReached() {
+        if (this.windowcounter < this.threshold) {
+            this.increaseCounter()
+            return false
+        }
+        return true
+    }
+    increaseCounter() {
+        this.windowcounter++
+    }
+    resetCounter() {
+        this.windowcounter = 0
+        this.windowTime = new Date().getTime()
     }
 }
 
-module.exports=WindowCounter
+module.exports = WindowCounter
