9f2ba6bf9da99a7719e881839c81b623cd64d933
Kalayu-Ftsum <kalayuftsumofficial@gmail.com>
2023-10-02 16:12:33 +0200

    Fixed forgotten middleware on tokenbuckets

--- src/routes/tokenBuckets.js
+++ src/routes/tokenBuckets.js
@@ -1,5 +1,6 @@
 const router=require('express').Router()
-
+const tokenBucketsMiddleware=require('../middlewares/tokenBucket')
+router.use(tokenBucketsMiddleware)
 router.get('/limited',(req, res) => {
     res.send('Limited, don\'t over use me!')
 })
